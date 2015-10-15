94b0929da5072bc365446cf448ec051fe53232f7
agomezcastro <agomezcastro@atanasoff05.danielcastelao.org>
2015-10-15 10:42:52 +0200

    ejercicio de clases añadido valores

--- src/exemplo2/Exemplo2.java
+++ src/exemplo2/Exemplo2.java
@@ -1,6 +1,8 @@
 
 package exemplo2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author agomezcastro
@@ -9,10 +11,17 @@ public class Exemplo2 {
 
     
     public static void main(String[] args) {
-        Rectangulo rectangulo1 = new Rectangulo(3,5);
+        
+        float base, altura;
+        Scanner obx=new Scanner (System.in);
+        System.out.println("introduce la base: ");
+        base= obx.nextFloat();
+        System.out.println("introduce la altura: ");
+        altura= obx.nextFloat();
+        Rectangulo rectangulo1 = new Rectangulo(base, altura);
         System.out.println("base: "+ rectangulo1.getBase());
         System.out.println("altura: "+ rectangulo1.getAltura());
-        System.out.println("area: "+ rectangulo1.calcularArea(3, 5));
+        System.out.println("area: "+ rectangulo1.calcularArea(base, altura));
         System.out.println("perimetro: "+ rectangulo1.calcularPerimetro());
     }
     
